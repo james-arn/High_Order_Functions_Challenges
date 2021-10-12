3a7227d85f61a2e7c030a340e719357e7a1574f4
James Arnold <j1mesu@hotmail.co.uk>
2021-10-12 17:31:04 +0100

    challenges 1-3

---
+++ High_Order_Functions_Challenges.js
@@ -0,0 +1,36 @@
+//Challenge 1
+//simple
+let codeNation1 = () => {
+        console.log("Hello Code Nation")
+    }
+//extended
+let codeNation5 = (reps) => {
+    for (i=0; i < reps; i++){
+        console.log("Hello Code Nation")
+    }
+}
+codeNation1()
+codeNation5(5)
+
+//Challenge 2
+//simple
+let myAdd = (num1, num2) => {
+    return num1 + num2
+}
+console.log(myAdd(1,2))
+//extended
+
+let myAdd = (reps, num1, num2) => {
+    myArray = []
+    for (let i = 0; i < reps; i++) {
+      myArray.push(num1+num2)
+    } 
+    const reducer = (a,b)=>a+b
+    return myArray.reduce(reducer)
+}
+console.log(myAdd(5, 1, 2));
+
+// challenge 3 - use map to iterate through an array & multiply each array item by 3.
+let myArr = [1,2,3,4,5];
+const map1 = myArr.map(x => x * 3);
+console.log(map1)
